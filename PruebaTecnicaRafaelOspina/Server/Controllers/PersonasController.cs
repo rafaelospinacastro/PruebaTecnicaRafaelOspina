60ea7a807d3aff4f8a5cb32c875f1b18100dd6ca
rafaelospinacastro <81756302+rafaelospinacastro@users.noreply.github.com>
2022-12-22 17:39:20 -0500

    Subida Inicial

---
+++ PruebaTecnicaRafaelOspina/Server/Controllers/PersonasController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PruebaDoubleV.Server.Models;
+using PruebaDoubleV.Shared;
+
+namespace PruebaDoubleV.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        private readonly PruebaDoubleVContext _context;
+
+        public PersonasController(PruebaDoubleVContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Personas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Persona>>> GetPersonas()
+        {
+            return await _context.Personas.ToListAsync();
+        }
+
+        // GET: api/Personas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Persona>> GetPersona(int id)
+        {
+            var persona = await _context.Personas.FindAsync(id);
+
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            return persona;
+        }
+
+        // PUT: api/Personas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPersona(int id, Persona persona)
+        {
+            if (id != persona.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(persona).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Personas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        /*[HttpPost]
+        public async Task<ActionResult<Persona>> PostPersona(Persona persona)
+        {
+            _context.Personas.Add(persona);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPersona", new { id = persona.Id }, persona);
+        }*/
+        [HttpPost]
+        public async Task<ActionResult<bool>> CrearPersona(PersonaDTO.CrearPersona personaVal)
+        {
+            try
+            {
+                var persona = new Persona
+                {
+                    Apellidos = personaVal.Apellidos,
+                    Email = personaVal.Email,
+                    FechaCreacion = DateTime.Now,
+                    Nombres = personaVal.Nombres,
+                    NumeroIdentificacion = personaVal.NumeroIdentificacion,
+                    TipoIdentificacion = personaVal.TipoIdentificacion
+                };
+                _context.Personas.Add(persona);
+                await _context.SaveChangesAsync();
+
+                return Ok(true);
+
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("Se presento el siguiente problema " + ex.Message);
+
+            }
+            
+        }
+
+        // DELETE: api/Personas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePersona(int id)
+        {
+            var persona = await _context.Personas.FindAsync(id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            _context.Personas.Remove(persona);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PersonaExists(int id)
+        {
+            return _context.Personas.Any(e => e.Id == id);
+        }
+    }
+}
